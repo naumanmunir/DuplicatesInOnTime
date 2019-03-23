cf15f7db0148f7af58379cdf6aa985dee294bf27
Nonabona <bloodo9@hotmail.com>
2019-03-23 16:26:12 -0400

    First

--- DuplicatesInOnTime/Program.cs
+++ DuplicatesInOnTime/Program.cs
@@ -10,6 +10,31 @@ namespace DuplicatesInOnTime
     {
         static void Main(string[] args)
         {
+            int[] arr2 = { 1, 2, 3, 1, 3, 6, 6 };
+
+            FindDuplicates(arr2);
+
+            Console.ReadLine();
+        }
+
+        public static int[] FindDuplicates(int[] arr)
+        {
+            List<int> dups = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[Math.Abs(arr[i])] >= 0)
+                {
+                    arr[Math.Abs(arr[i])] = -arr[Math.Abs(arr[i])];
+                }
+                else
+                {
+                    dups.Add(Math.Abs(arr[i]));
+                }
+            }
+
+
+            return dups.ToArray();
         }
     }
 }

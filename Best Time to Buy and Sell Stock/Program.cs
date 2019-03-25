e87b12a5e5102fc48d954d5dcc29cc8ad2eca498
Nonabona <bloodo9@hotmail.com>
2019-03-24 22:36:28 -0400

    added notes

--- Best Time to Buy and Sell Stock/Program.cs
+++ Best Time to Buy and Sell Stock/Program.cs
@@ -10,19 +10,21 @@ namespace Best_Time_to_Buy_and_Sell_Stock
     {
         static void Main(string[] args)
         {
-            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            int[] prices = { 1,2};
 
             MaxProfit(prices);
         }
 
+        //Naive way of doing it
+        //Time complexity: O(n^2), because of 2 nested loops
+        //Space complexity: O(1), only one variable is used
         static int MaxProfit(int[] prices)
         {
             int profit = 0;
-            int size = prices.Length;
 
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < prices.Length; i++)
             {
-                for (int j = i+1; j < size - 1; j++)
+                for (int j = i+1; j < prices.Length; j++)
                 {
                     if (prices[j] - prices[i] > profit)
                     {

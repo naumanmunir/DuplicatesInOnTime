5b45534008c9e6598fe3e7afc269519a1ee38562
Nonabona <bloodo9@hotmail.com>
2019-04-27 20:29:24 -0400

    questions

---
+++ Happy Number/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Happy_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = IsHappy(7);
+
+        }
+
+        static bool IsHappy(int n)
+        {
+            List<int> sumSeen = new List<int>();
+
+            if (n == 0) return false;
+
+            int sum = n;
+
+            while (sum != 1)
+            {
+
+                if (sum >= 10 && sum < 100)
+                {
+                    var rightNum = sum % 10;
+                    var leftNum = sum / 10;
+
+                    sum = (rightNum * rightNum) + (leftNum * leftNum);
+
+                    if (sum == 1) break;
+
+                    if (!sumSeen.Contains(sum))
+                    {
+                        sumSeen.Add(sum);
+                    }
+                    else return false;
+                }
+                if (sum < 10)
+                {
+                    sum *= sum;
+                    if (sum == 1) break;
+
+                    if (!sumSeen.Contains(sum))
+                    {
+                        sumSeen.Add(sum);
+                    }
+                    else return false;
+                }
+                if (sum >= 100)
+                {
+
+                    int tmp = 0;
+
+                    while (sum > 0)
+                    {
+                        var num = sum % 10;
+
+                        sum = sum / 10;
+
+                        tmp += num * num;
+
+                    }
+
+                    sum = tmp;
+                    if (sum == 1) break;
+                    if (!sumSeen.Contains(sum))
+                    {
+                        sumSeen.Add(sum);
+                    }
+                    else return false;
+                }
+
+            }
+
+
+            return true;
+        }
+    }
+}
---
+++ Implement Queue using Stacks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implement_Queue_using_Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Push(1);
+            q.Push(2);
+            Console.WriteLine(q.Peek());
+            q.Pop();
+            q.Empty();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Queue
+    {
+        public List<int> stack { get; set; } = new List<int>();
+
+        public Queue()
+        {
+
+        }
+
+        /** Push element x to the back of queue. */
+        public void Push(int x)
+        {
+            stack.Add(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop()
+        {
+            var res = stack[0];
+
+            stack.RemoveAt(0);
+
+            return res;
+        }
+
+        /** Get the front element. */
+        public int Peek()
+        {
+            return stack[0];
+        }
+
+        /** Returns whether the queue is empty. */
+        public bool Empty()
+        {
+            return stack.Count == 0 ? true : false;
+        }
+    }
+}

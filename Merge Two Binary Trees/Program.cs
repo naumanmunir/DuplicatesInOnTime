efe5d7e590ee3477deca7980de5ce7395d6b9946
Nonabona <bloodo9@hotmail.com>
2019-04-28 22:26:11 -0400

    question

--- Merge Two Binary Trees/Program.cs
+++ Merge Two Binary Trees/Program.cs
@@ -12,18 +12,14 @@ namespace Merge_Two_Binary_Trees
         static void Main(string[] args)
         {
             TreeNode tn = new TreeNode(1);
-            tn.left = new TreeNode(3);
-            tn.right = new TreeNode(2);
-            tn.left.left = new TreeNode(5);
 
-            TreeNode tn2 = new TreeNode(2);
-            tn2.left = new TreeNode(1);
-            tn2.right = new TreeNode(3);
-            tn2.right.right = new TreeNode(7);
-            tn2.left.right = new TreeNode(4);
+            TreeNode tn2 = new TreeNode(1);
+            tn2.left = new TreeNode(2);
+
 
             var res = MergeTrees(tn,tn2);
         }
+
         static TreeNode MergeTrees(TreeNode t1, TreeNode t2)
         {
             var curr1 = t1;
@@ -33,8 +29,11 @@ namespace Merge_Two_Binary_Trees
 
             while (curr2 != null || curr1 != null || stack.Count > 0)
             {
-                while (curr2 != null || curr1 != null)
+                while (curr1 != null || curr2 != null)
                 {
+                    if (curr1 != null && curr2 != null)
+                        curr1.val += curr2.val;
+
                     if (curr1 != null)
                     {
                         stack.Push(curr1);
@@ -55,29 +54,91 @@ namespace Merge_Two_Binary_Trees
                     }
 
                 }
-
-                curr1 = stack.Pop();
+                
                 curr2 = stack.Pop();
+                curr1 = stack.Pop();
 
-                curr2.val += curr1.val;
+                var tmpcurr1 = curr1.right;
+                var tmpcurr2 = curr2.right;
+
+
+                if (tmpcurr1 == null && tmpcurr2 != null)
+                {
+                    curr1.right = new TreeNode(0);
+                }
+                if (tmpcurr2 == null && tmpcurr1 != null)
+                {
+                    curr2.right = new TreeNode(0);
+                }
+
+                
+
 
                 curr1 = curr1.right;
                 curr2 = curr2.right;
 
-                if (curr1 == null && curr2 != null)
-                {
-                    curr1 = new TreeNode(0);
-                }
-                if (curr2 == null && curr1 != null)
-                {
-                    curr2 = new TreeNode(0);
-                }
             }
-
-
-
 
             return t1;
         }
+
+        //static TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        //{
+        //    var curr1 = t1;
+        //    var curr2 = t2;
+
+        //    Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        //    while (curr2 != null || curr1 != null || stack.Count > 0)
+        //    {
+        //        while (curr1 != null || curr2 != null)
+        //        {
+        //            if (curr1 != null)
+        //            {
+        //                stack.Push(curr1);
+        //                curr1 = curr1.left;
+        //            }
+        //            else
+        //            {
+        //                stack.Push(new TreeNode(0));
+        //            }
+        //            if (curr2 != null)
+        //            {
+        //                stack.Push(curr2);
+        //                curr2 = curr2.left;
+        //            }
+        //            else
+        //            {
+        //                stack.Push(new TreeNode(0));
+        //            }
+
+        //        }
+
+        //        curr2 = stack.Pop();
+        //        curr1 = stack.Pop();
+
+        //        var tmpcurr1 = curr1.right;
+        //        var tmpcurr2 = curr2.right;
+
+
+        //        if (tmpcurr1 == null && tmpcurr2 != null)
+        //        {
+        //            curr1.right = new TreeNode(0);
+        //        }
+        //        if (tmpcurr2 == null && tmpcurr1 != null)
+        //        {
+        //            curr2.right = new TreeNode(0);
+        //        }
+
+        //        curr1.val += curr2.val;
+
+
+        //        curr1 = curr1.right;
+        //        curr2 = curr2.right;
+
+        //    }
+
+        //    return t1;
+        //}
     }
 }
---
+++ String Compression/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compress(new char {'' });
+        }
+
+        static int Compress(char[] chars)
+        {
+
+            int count = 1;
+
+            for (int i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i + 1] == chars[i])
+                    count++;
+                else
+                {
+                    if (count > 1)
+                        chars[i] = Convert.ToChar(count);
+
+                    count = 1;
+                }
+            }
+
+
+            return chars.Length;
+        }
+    }
+}
